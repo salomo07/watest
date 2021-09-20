6b5b2cfc1e7ee3d13eaccb26dd1bc03ccc90048a
salomo07 <kenjibanzai@gmail.com>
2021-09-20 11:48:43 +0700

    xxx

--- application/models/M_wa.php
+++ application/models/M_wa.php
@@ -9,6 +9,10 @@ class M_wa extends CI_Model {
     function insertConversation($data) {
         $this->db->insert('active_conversation', $data);
     }
+    function updateConversation($data) {
+        $this->db->where('number', $data['number']);
+        $this->db->update('active_conversation', $data);
+    }
     function checkActiveConversation($no) {
         if(substr($no, 0, 2)=="62"){$no="0".substr($no, 2);}
         $query= $this->db->query("select * from active_conversation where number='$no' and endtime IS NULL");
