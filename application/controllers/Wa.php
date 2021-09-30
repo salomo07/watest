726836b172ca19effb745cda862a503735fc3c84
salomo07 <kenjibanzai@gmail.com>
2021-09-30 23:50:59 +0700

    WA Interactive button

--- application/controllers/Wa.php
+++ application/controllers/Wa.php
@@ -157,7 +157,7 @@ class Wa extends CI_Controller
                 }
                 else{
                     $this->sendingTextMsg($this->formatingNumber($val->from),$msg->text);
-                    $this->sendingTextMsg($this->formatingNumber($val->from),"End by client ".$msg->text);
+                    $this->sendingTextMsg($this->formatingNumber($val->from),"End by client ".json_encode($msg->text));
                     if($msg->type=="TEXT" || $msg->type=="INTERACTIVE_BUTTON_REPLY")
                     {
                         if($msg->text=="#out")
